bb7fed1a165d2d2dc62d3e951de6f2fe1495bc61
Vital Zanka <vital.zanko@gmail.com>
2021-01-30 20:31:22 +0300

    use Bootique 2.0

--- json4img-rest/src/main/java/com/github/vitalz/jrest/json4img/Json4ImgApplication.java
+++ json4img-rest/src/main/java/com/github/vitalz/jrest/json4img/Json4ImgApplication.java
@@ -1,6 +1,5 @@
 package com.github.vitalz.jrest.json4img;
 
-import com.google.inject.Module;
 import io.bootique.Bootique;
 import io.bootique.jersey.JerseyModule;
 
@@ -8,13 +7,11 @@ public class Json4ImgApplication {
 
     public static void main(String[] args) {
 
-        Module module = binder -> {
-            JerseyModule.extend(binder).addResource(Json4ImgApi.class);
-        };
-
         Bootique
-                .app("")
-                .module(module)
+                .app(args)
+                .module(binder -> {
+                    JerseyModule.extend(binder).addResource(Json4ImgApi.class);
+                })
                 .autoLoadModules()
                 .exec()
                 .exit();
