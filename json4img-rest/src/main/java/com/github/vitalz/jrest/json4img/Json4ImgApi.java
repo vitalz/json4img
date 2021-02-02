eebd74d8045ea57b2cd30fe80e4677e8ee3c2b08
Vital Zanko <vitalz@objectstyle.com>
2021-02-02 14:46:04 +0300

    rollback to previous Bootique version; runs in Dockerization

--- json4img-rest/src/main/java/com/github/vitalz/jrest/json4img/Json4ImgApi.java
+++ json4img-rest/src/main/java/com/github/vitalz/jrest/json4img/Json4ImgApi.java
@@ -5,14 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Path("/j4i")
 public class Json4ImgApi {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String get() {
-        return "{\"message\":\"it works!\"}";
+        return "{\"message\":\"Json for Image works!\"}";
     }
-
 
 }
--- json4img-rest/src/main/java/com/github/vitalz/jrest/json4img/Json4ImgApplication.java
+++ json4img-rest/src/main/java/com/github/vitalz/jrest/json4img/Json4ImgApplication.java
@@ -1,18 +1,26 @@
 package com.github.vitalz.jrest.json4img;
 
+import com.google.inject.Binder;
+import com.google.inject.Module;
 import io.bootique.Bootique;
 import io.bootique.jersey.JerseyModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Json4ImgApplication {
+public class Json4ImgApplication implements Module {
+    private final static Logger log = LoggerFactory.getLogger(Json4ImgApplication.class);
 
     public static void main(String[] args) {
-
-        Bootique
-                .app(args)
+        Bootique.app(args)
                 .autoLoadModules()
-                .module(binder -> JerseyModule.extend(binder).addResource(Json4ImgApi.class))
+                .module(Json4ImgApplication.class)
                 .exec()
                 .exit();
     }
 
+    @Override
+    public void configure(Binder binder) {
+        JerseyModule.extend(binder)
+                .addResource(Json4ImgApi.class);
+    }
 }
